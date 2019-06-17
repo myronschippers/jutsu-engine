bbd29da371e10aca486e0fa30b0580d50162b122
Myron <dubtwee@gmail.com>
2019-06-17 14:21:48 -0500

    ensuring that all of the new components and the new splash page are registered appropriately

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,19 @@ import {
 
 import {connect} from 'react-redux';
 
+// STYLING
+import './App.css';
+
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
+// PAGE ROUTE COMPONENTS
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import HomePage from '../HomePage/HomePage';
-
-import './App.css';
 
 class App extends Component {
   componentDidMount () {
@@ -31,32 +33,13 @@ class App extends Component {
         <div>
           <Nav />
 
-          <h2 class="testFont testFont_alfaSlabOne">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-          <h2 class="testFont testFont_cabin">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-          <h2 class="testFont testFont_daysOne">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-          <h2 class="testFont testFont_didactGothic">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-          <h2 class="testFont testFont_oxygen">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-          <h2 class="testFont testFont_fredericka">
-              Sample Jutsu text for testing fonts. VERY LOUD!!!
-          </h2>
-
           <Switch>
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
+            {/* <Redirect exact from="/" to="/home" /> */}
 
             <Route
               exact
-              path="/home"
+              path="/"
               component={HomePage}
             />
 
